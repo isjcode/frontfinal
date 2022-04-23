0f79332a7b9f5d98f4a3adcedbcf4acdc24029f8
enticies <lasauthr@protonmail.com>
2022-04-23 18:08:48 +0200

    Dummy.

---
+++ scripts/script.js
@@ -0,0 +1,64 @@
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+let btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+$(document).ready(function () {
+  $(".category-btn").click(function () {
+    $(".drop-categories").slideToggle("fast")
+  });
+});
+
+$('.single-item').slick({
+  autoplay: true,
+  arrows: false,
+  autoplaySpeed: 1000,
+});
+
+var countDownDate = new Date("April 24, 2022 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = `<span class="time"> ${days} </span> : <span class="time">${hours} </span> : <span class='time'>${minutes} </span> : <span class="time"> ${seconds} </span>`;
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
